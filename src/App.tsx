525932bea4b92050ef886559b389330e836d87e6
Mykola Bikchentayev <ironhead749@gmail.com>
2026-07-07 19:04:12 +0300

    feat: wire up app shell, routing and static assets
    React entry point, router configuration and public assets. App now
    builds and runs end to end.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { Toaster } from "@/components/ui/sonner";
+import { HomePage } from "@/pages/HomePage/HomePage";
+import { RoomPage } from "@/pages/RoomPage/RoomPage";
+
+import { FOLDER_ROUTE_PATTERN, HOME_PATH } from "@/lib/routes";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={HOME_PATH} element={<HomePage />} />
+        <Route path={FOLDER_ROUTE_PATTERN} element={<RoomPage />} />
+        <Route path="*" element={<Navigate to={HOME_PATH} replace />} />
+      </Routes>
+      <Toaster position="bottom-right" />
+    </BrowserRouter>
+  );
+}
